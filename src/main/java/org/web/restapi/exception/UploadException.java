78f27e565d9f5966ed491524931115a3144ddf70
Crischinaldo <temp566566@gmx.de>
2020-08-08 10:41:49 +0200

    add exc for upload related errors

---
+++ src/main/java/org/web/restapi/exception/UploadException.java
@@ -0,0 +1,10 @@
+package org.web.restapi.exception;
+
+import java.io.IOException;
+
+public class UploadException extends IOException {
+
+    public UploadException(String message) {
+        super(message);
+    }
+}
