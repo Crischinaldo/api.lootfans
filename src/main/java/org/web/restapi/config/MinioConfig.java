7cfb9b05a8fa14ef326af16f9a9d91de58d19d32
Crischinaldo <temp566566@gmx.de>
2020-08-08 12:35:09 +0200

    minio client configuration

---
+++ src/main/java/org/web/restapi/config/MinioConfig.java
@@ -0,0 +1,29 @@
+package org.web.restapi.config;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioConfig {
+
+    @Value("${minio.access.name}")
+    String accessKey;
+    @Value("${minio.access.secret}")
+    String accessSecret;
+    @Value("${minio.url}")
+    String endPoint;
+
+
+    @Bean
+    public MinioClient minioClient() {
+
+        return MinioClient.builder()
+                .endpoint(endPoint)
+                .credentials(accessKey, accessSecret)
+                .build();
+    }
+
+}
+
