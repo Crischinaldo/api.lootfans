57e70a43555600d0d97b20a958c6f1cc6fcc4b41
Crischinaldo <temp566566@gmx.de>
2020-08-11 00:35:39 +0200

    add keycloak admin client

--- src/main/java/de/lootfans/restapi/config/KeycloakSecurityConfig.java
+++ src/main/java/de/lootfans/restapi/config/KeycloakSecurityConfig.java
@@ -1,12 +1,19 @@
 package de.lootfans.restapi.config;
 
 
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.keycloak.OAuth2Constants;
 import org.keycloak.adapters.KeycloakConfigResolver;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -64,8 +71,38 @@ public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter
 
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("api/v1/files").hasRole("user") // only user with role user are allowed to access
-                .antMatchers("api/v1/users").hasRole("user") // only user with role user are allowed to access
+            //    .antMatchers("api/v1/files").hasRole("user") // only user with role user are allowed to access
+           //     .antMatchers("api/v1/users").hasRole("admin") // only user with role user are allowed to access
                 .anyRequest().permitAll();
+    }
+
+
+    @Value("${keycloakadmin.realm}")
+    String realm;
+    @Value("${keycloakadmin.client}")
+    String client;
+    @Value("${keycloakadmin.credentials.secret}")
+    String secret;
+    @Value("${keycloak.auth-server-url}")
+    String serverUrl;
+
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(KeycloakSecurityConfig.class);
+
+
+    @Bean
+    public Keycloak keycloakClient() {
+
+        LOGGER.info("Initalizing keycloak Admin Client with these credentials:\n" +
+                        "realm: {}\nclient: {}\nsecret: {}\nserverurl: {}\n",
+                realm, client, secret, serverUrl);
+
+        return KeycloakBuilder
+                .builder()
+                .serverUrl(serverUrl)
+                .realm(realm)
+                .clientId(client)
+                .clientSecret(secret)
+                .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
+                .build();
     }
 }
