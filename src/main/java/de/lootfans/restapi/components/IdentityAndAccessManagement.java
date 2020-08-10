04e3662bf21e266c4841512868644b2772f49fdb
Crischinaldo <temp566566@gmx.de>
2020-08-10 09:12:50 +0200

    add interface for iam

---
+++ src/main/java/de/lootfans/restapi/components/IdentityAndAccessManagement.java
@@ -0,0 +1,12 @@
+package de.lootfans.restapi.components;
+
+import java.util.HashMap;
+
+public interface IdentityAndAccessManagement<T, G> {
+
+    public String createUser(T user);
+
+    public G getUserById(String userId);
+
+    public void setPassword(T user, String password);
+}
