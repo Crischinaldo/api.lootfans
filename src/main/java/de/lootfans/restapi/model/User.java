eb4d08abb5d277a868e83fc9bcbd45c3d2f13ec7
Crischinaldo <temp566566@gmx.de>
2020-08-11 00:33:33 +0200

    getter/setter for userid

--- src/main/java/de/lootfans/restapi/model/User.java
+++ src/main/java/de/lootfans/restapi/model/User.java
@@ -85,13 +85,15 @@ public class User{
         this.age = age;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
+
+    
 
     public String getEmail() {
         return email;
