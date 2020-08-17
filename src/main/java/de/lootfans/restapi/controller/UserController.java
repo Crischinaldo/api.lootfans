65c745e5aa77a3d3ba2393cb6a47428b1cd775f8
Crischinaldo <temp566566@gmx.de>
2020-08-18 01:31:26 +0200

    add signin endpoint and gson tokenresponse

--- src/main/java/de/lootfans/restapi/controller/UserController.java
+++ src/main/java/de/lootfans/restapi/controller/UserController.java
@@ -1,5 +1,11 @@
 package de.lootfans.restapi.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import de.lootfans.restapi.config.KeycloakSecurityConfig;
+import de.lootfans.restapi.response.TokenResponse;
+import org.keycloak.KeycloakSecurityContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +18,6 @@ import de.lootfans.restapi.service.UserService;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1")
 public class UserController {
 
     @Autowired
@@ -38,20 +43,53 @@ public class UserController {
             produces = MediaType.APPLICATION_JSON_VALUE,
             value = "/users/{id}"
     )
+
     @ResponseStatus(HttpStatus.OK)
-    public User getUserById(@PathVariable Long id) throws UserNotFoundException {
-        return userService.getUserById(id);
+    public ResponseEntity<User> getUserById(@PathVariable Long id) throws UserNotFoundException {
+        User usr = userService.getUserById(id);
+
+        if (usr != null) {
+            return new ResponseEntity<>(usr, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @RequestMapping(
             method = RequestMethod.POST,
             value = "/users",
             produces = MediaType.APPLICATION_JSON_VALUE,
-            consumes = MediaType.APPLICATION_JSON_VALUE
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE
     )
     @ResponseStatus(HttpStatus.CREATED)
-    public User addUser(@RequestBody User user) {
-        return userService.createUser(user);
+    public ResponseEntity<User> addUser(User user) {
+        User usr = userService.createUser(user);
+
+        if (usr != null) {
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            value = "/signin",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE
+    )
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<String> signInUser(String userName, String password) {
+
+        String token = userService.signInUser(userName, password);
+
+        Gson gson = new Gson();
+        TokenResponse response = new TokenResponse();
+        response.token = token;
+
+        String jsonString = gson.toJson(response);
+
+        return new ResponseEntity<>(jsonString, HttpStatus.CREATED);
     }
 
 }
