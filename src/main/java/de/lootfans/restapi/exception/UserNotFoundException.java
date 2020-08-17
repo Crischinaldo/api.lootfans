9bee4f98a007762131a37f3786b84e0c26869ad5
Crischinaldo <temp566566@gmx.de>
2020-08-17 17:31:17 +0200

    string constructor

--- src/main/java/de/lootfans/restapi/exception/UserNotFoundException.java
+++ src/main/java/de/lootfans/restapi/exception/UserNotFoundException.java
@@ -5,4 +5,8 @@ public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(long id) {
         super(String.format("User with Id %d not found", id));
     }
+
+    public UserNotFoundException(String name) {
+        super(String.format("User with Id %s not found", name));
+    }
 }
