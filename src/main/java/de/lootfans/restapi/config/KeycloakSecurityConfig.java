89f6270a3117c824659ec36fa3a64b4aeda13b60
Crischinaldo <temp566566@gmx.de>
2020-08-17 17:33:06 +0200

    implemented authzclient bean

--- src/main/java/de/lootfans/restapi/config/KeycloakSecurityConfig.java
+++ src/main/java/de/lootfans/restapi/config/KeycloakSecurityConfig.java
@@ -1,20 +1,26 @@
 package de.lootfans.restapi.config;
 
 
+import de.codecentric.boot.admin.server.web.client.HttpHeadersProvider;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.adapters.KeycloakConfigResolver;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootProperties;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.authorization.client.AuthzClient;
+import org.keycloak.authorization.client.Configuration;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,21 +33,39 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 
 
 @KeycloakConfiguration
-@EnableWebSecurity
+@EnableConfigurationProperties(KeycloakSpringBootProperties.class)
 public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
+    @Value("${keycloakadmin.realm}")
+    String realm;
+    @Value("${keycloakadmin.client}")
+    String client;
+    @Value("${keycloakadmin.credentials.secret}")
+    String secret;
+    @Value("${keycloak.auth-server-url}")
+    String serverUrl;
+
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(KeycloakSecurityConfig.class);
+
+
     /**
-     * Registers the KeycloakAuthenticationProvider with the authentication manager.
+     * Use {@link KeycloakAuthenticationProvider}
+     *
+     * @param auth
+     * @throws Exception
      */
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        SimpleAuthorityMapper grantedAuthorityMapper = new SimpleAuthorityMapper();
+        grantedAuthorityMapper.setPrefix("ROLE_");
+        grantedAuthorityMapper.setConvertToUpperCase(true);
+
         KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
-
-        // adding proper authority mapper for prefixing role with "ROLE_"
-        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
-
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(grantedAuthorityMapper);
         auth.authenticationProvider(keycloakAuthenticationProvider);
     }
+
 
     /**
      * Provide a session authentication strategy bean which should be of type
@@ -70,27 +94,35 @@ public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter
         super.configure(http);
 
         http.cors().and().csrf().disable()
+                .sessionManagement()
+                .sessionAuthenticationStrategy(sessionAuthenticationStrategy())
+                .and()
                 .authorizeRequests()
-            //    .antMatchers("api/v1/files").hasRole("user") // only user with role user are allowed to access
+                .antMatchers("/files*").hasRole("user") // only user with role user are allowed to access
            //     .antMatchers("api/v1/users").hasRole("admin") // only user with role user are allowed to access
                 .anyRequest().permitAll();
     }
 
-
-    @Value("${keycloakadmin.realm}")
-    String realm;
-    @Value("${keycloakadmin.client}")
-    String client;
-    @Value("${keycloakadmin.credentials.secret}")
-    String secret;
-    @Value("${keycloak.auth-server-url}")
-    String serverUrl;
-
-    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(KeycloakSecurityConfig.class);
+    /**
+     * {@link HttpHeadersProvider} used to populate the {@link HttpHeaders} for
+     * accessing the state of the disovered clients.
+     *
+     * @param keycloak
+     * @return
+     */
+    @Bean
+    public HttpHeadersProvider keycloakBearerAuthHeaderProvider(Keycloak keycloak) {
+        return (app) -> {
+            String accessToken = keycloak.tokenManager().getAccessTokenString();
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
+            return headers;
+        };
+    }
 
 
     @Bean
-    public Keycloak keycloakClient() {
+    public Keycloak keycloakAdminClient() {
 
         LOGGER.info("Initalizing keycloak Admin Client with these credentials:\n" +
                         "realm: {}\nclient: {}\nsecret: {}\nserverurl: {}\n",
@@ -105,4 +137,20 @@ public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter
                 .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
                 .build();
     }
+
+    @Bean
+    public AuthzClient keycloakUserClient() {
+
+        LOGGER.info("Initalizing keycloak User Client with these credentials:\n" +
+                        "realm: {}\nclient: {}\nsecret: {}\nserverurl: {}\n",
+                realm, client, secret, serverUrl);
+
+        return AuthzClient.create();
+
+
+
+
+
+    }
+
 }
